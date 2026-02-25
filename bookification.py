587c88b4c1c9a5ad4be35ee971e7997456030296
ian <jorqueraian@gmail.com>
2026-02-25 09:18:54 -0700

    updates to bookification

--- bookification.py
+++ bookification.py
@@ -6,8 +6,25 @@ import sys
 import re
 
 
-input_pdf = r"C:/Users/jorqu/Downloads/The Metamorphosis.pdf"
-page_scaling = 1
+input_pdf = None
+page_scaling = 1.1
+blanks_at_start = 0
+blanks_at_end = 0
+
+if len(sys.argv) > 1 and sys.argv[1] != "-h":
+    input_pdf = sys.argv[1]
+    if len(sys.argv) > 2:
+        page_scaling = float(sys.argv[2])
+        if len(sys.argv) > 3:
+            blanks_at_start = int(sys.argv[3])
+            if len(sys.argv) > 4:
+                blanks_at_end = int(sys.argv[4])
+elif input_pdf is None or (len(sys.argv) > 1 and sys.argv[1] == "-h"):
+    print(f"It seems like you need help! <> indicate required fields, and [] indicate optional fields.\nbookification <input pdf file> [page scaling = 1.1] [blanks at start = 0] [blanks at end = 0]")
+    sys.exit()
+
+total_blanks = blanks_at_start + blanks_at_end
+
 
 #if len(sys.argv) > 1 and sys.argv[1] != "-h":
 #    input_pdf = sys.argv[1]
@@ -28,16 +45,22 @@ remainder = 0
 sheets_per_signature = 8
 print("Options:")
 while sheets_per_signature > 2:
-    remainder = (num_pages+4) % (sheets_per_signature*4)
-    num_signatures = int((num_pages+4)//(sheets_per_signature*4))
+    remainder = (num_pages+total_blanks) % (sheets_per_signature*4)
+    num_signatures = int((num_pages+total_blanks)//(sheets_per_signature*4))
     if remainder > 0:
         num_signatures += 1
     print(f"Sheets per signature: {sheets_per_signature}. Number of signatures: {num_signatures}. Blank pages at the end: {0 if remainder == 0 else (sheets_per_signature*4 - remainder)}")
     sheets_per_signature -= 1
 
-sheets_per_signature = int(input("Please select the desired number of sheets per signatures: "))
-remainder = (num_pages+4) % (sheets_per_signature*4)
-num_signatures = int((num_pages+4)//(sheets_per_signature*4))
+
+input_text = input("Please select the desired number of sheets per signatures(leave blank for 1 signature): ")
+if input_text == '':
+    input_text = (num_pages + ((4-(num_pages % 4))%4))//4
+    
+sheets_per_signature = int(input_text)
+
+remainder = (num_pages+total_blanks) % (sheets_per_signature*4)
+num_signatures = int((num_pages+total_blanks)//(sheets_per_signature*4))
 if remainder > 0:
     num_signatures += 1
 
@@ -45,7 +68,7 @@ total_pages_in_book = sheets_per_signature*4*num_signatures
 
 # Add two blanks at the start and end, and how ever many 
 # is needed to make the entire thing divisible by  4
-reader_pages = ([None]*2) + list(reader.pages) + ([None]*2)  + ([None]*(total_pages_in_book-(num_pages+4)))
+reader_pages = ([None]*blanks_at_start) + list(reader.pages) + ([None]*blanks_at_end)  + ([None]*(total_pages_in_book-(num_pages+total_blanks)))
 num_pages = total_pages_in_book
 pages_per_signature = sheets_per_signature*4
 
@@ -55,8 +78,8 @@ page_width = 612 # int(reader_pages[0].mediabox.width)
 page_height = 792 # int(reader_pages[1].mediabox.height)
 
 # this is from the input
-input_page_width = int(reader_pages[5].mediabox.width)
-input_page_height = int(reader_pages[5].mediabox.height)
+input_page_width = int(reader_pages[blanks_at_start].mediabox.width)
+input_page_height = int(reader_pages[blanks_at_start].mediabox.height)
 
 
 # This the number of printed pages per signature
@@ -73,8 +96,10 @@ for sig in range(num_signatures):
             page_1 = reader_pages[first_page_in_sig+pages_per_signature-i-1]
         
 
-        scale_factor = (page_height/2)/input_page_width
+        scale_factor = page_scaling*(page_height/2)/input_page_width
         new_height = scale_factor*input_page_height
+        
+        side_margins = ((page_height/2)-scale_factor*input_page_width)/2
         topbottom_margins = (page_width-new_height)/2
 
         translated_page = PageObject.create_blank_page(None, page_width, page_height)
@@ -82,12 +107,12 @@ for sig in range(num_signatures):
         if page_1 is not None:
             temp_page = PageObject.create_blank_page(None, page_width, page_height)
             temp_page.merge_page(page_1)
-            temp_page.add_transformation(Transformation().scale(scale_factor,scale_factor).rotate(-90).translate(topbottom_margins,page_height))#.translate(0, page_height))
+            temp_page.add_transformation(Transformation().scale(scale_factor,scale_factor).rotate(-90).translate(topbottom_margins,page_height-side_margins))#.translate(0, page_height))
             translated_page.merge_page(temp_page)
         if page_2 is not None:
             temp_page = PageObject.create_blank_page(None, page_width, page_height)
             temp_page.merge_page(page_2)
-            temp_page.add_transformation(Transformation().scale(scale_factor,scale_factor).rotate(-90).translate(topbottom_margins,page_height/2))
+            temp_page.add_transformation(Transformation().scale(scale_factor,scale_factor).rotate(-90).translate(topbottom_margins,page_height/2-side_margins))
             translated_page.merge_page(temp_page)
         print(f"added pages {first_page_in_sig+i}, {first_page_in_sig+pages_per_signature-i-1}")
         writer.add_page(translated_page)
